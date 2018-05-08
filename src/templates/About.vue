0ff0b309096abfb8190f8b00a10f3b6fa10e752e
thirozumi <thirozumi@gmail.com>
2018-05-08 16:36:55 +0900

    wip

--- src/templates/About.vue
+++ src/templates/About.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   div
     Header
-    h1 {{ title }}
+    h1 {{ title_primary }}
+      small {{ title_secondary }}
     p {{ lead }}
-    h2 CREDIT
-    ul
-      li(v-for='(val, key) in credit') {{ val.role }}: {{ val.name }}
+    p(v-html='button_lead')
+    p
+      button {{ button_text }}
     Footer
 </template>
 
@@ -20,24 +21,18 @@ export default {
   },
   data () {
     return {
-      title: '「ナビスコラ」とは？',
-      lead: '学術分野間の相互理解促進を目的とし、各分野の価値観(研究感、世界 観、評価基準など)、および行動特性(研究スタイル、研究コミュニティ活動など)を各分野ごとに調査し比較します。調査結果は、分析結果とともに公開とし、WEB掲載の他、冊子作成やシンポジウム開催等を行います。',
-      credit: [
-        {
-          role: 'Producer',
-          name: 'Karen R. Garza'
-        }, {
-          role: 'Art Director',
-          name: 'Ronald K. Kirschner'
-        }, {
-          role: 'Data Scientist',
-          name: 'Michael C. Conner'
-        }, {
-          role: 'Web Development',
-          name: 'Troy R. Gower'
-        }
-      ]
+      title_primary: 'About Navischola',
+      title_secondary: 'ナビスコラとは',
+      lead: '京都大学・学際融合教育研究推進センターが、学術分野間の相互理解促進を目的とし実施している「学術分野の文化比較大調査」。各研究分野の価値観（研究感、世界観、評価基準など）、および行動特性（研究スタイル、研究コミュニティ活動など）をデータ化した日本初のデータベースが、着々と出来上がりつつあります。『ナビスコラ』では、2016年3月18日現在のデータを用い、誰でも簡単に「あなたのための学問」を診断できるようにしました。また、現状の学問分野の状況をヴィジュアライゼーションした「知の見取り図」も掲載。いまの学問を知るためのツールとしてご活用ください。',
+      button_lead: '「学術分野の文化比較大調査」は絶賛データ募集中。<br>研究者の方は、是非アンケートにご協力ください。',
+      button_text: 'アンケートに協力する'
     }
   }
 }
 </script>
+
+<style lang="scss">
+  h1 > small {
+    display: block;
+  }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './assets'),
+    publicPath: '/assets/',
     filename: 'build.js'
   },
   module: {
